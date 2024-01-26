d9f2b09fda80f5798b9af5f3b06756164e3300f9
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-01-26 21:12:37 +0200

    fix: do not show clear button when query is empty

--- lib/src/widgets/country_selector/country_selector_page.dart
+++ lib/src/widgets/country_selector/country_selector_page.dart
@@ -83,10 +83,11 @@ class CountrySelectorSearchDelegate extends SearchDelegate<Country> {
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      IconButton(
-        onPressed: () => query = '',
-        icon: const Icon(Icons.clear),
-      ),
+      if (query.isNotEmpty)
+        IconButton(
+          onPressed: () => query = '',
+          icon: const Icon(Icons.clear),
+        ),
     ];
   }
 
